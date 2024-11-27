af5b7618ca00ee809a81ab8412a5133e1f1b3a53
Julien Schiano <ext.julien.schianodicola@sncf.fr>
2024-11-27 16:18:17 +0100

    correct cdn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import {Logger, RadixDappToolkit, RadixNetwork} from "@radixdlt/radix-dapp-toolkit";
 
 const App = () => {
 // Initialisation du RadixDappToolkit
 
+  const rdt = RadixDappToolkit({
+    dAppDefinitionAddress:
+      'account_rdx128uazakd5z2707m8t640swdvce3wnc4h8f38wmmq7fmqhdetcdpytu',
+    networkId: RadixNetwork.Mainnet,
+    applicationName: 'Radix Web3 dApp',
+    applicationVersion: '1.0.0',
+    logger: Logger(3),
+  })
+  console.log(rdt);
   return (
     <div>
     </div>);
